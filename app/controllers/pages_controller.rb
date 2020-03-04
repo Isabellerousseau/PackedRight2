847ad2c04c8b2b6f1727e6181fb54e65d9785502
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-03-04 14:23:37 +0100

    mapbox including categories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,25 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    set_markers
+  end
+
+  def set_markers
+    @drivers = policy_scope(Driver)
+    @markers = @drivers.map do |driver|
+      {
+        lat: driver.latitude,
+        lng: driver.longitude,
+        infoWindow: render_to_string(partial: "drivers/info_window", locals: { driver: driver }),
+        image_url:
+        if driver.category == 'Car'
+          helpers.asset_url('car.png')
+        elsif driver.category == 'Bike'
+          helpers.asset_url('bike.png')
+        else
+          helpers.asset_url('bus.png')
+        end
+      }
+    end
   end
 end
