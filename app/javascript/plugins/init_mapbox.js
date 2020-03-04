6cda361c2271cb3abd0733bbeeb51fd0a8c28047
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-03-04 12:56:53 +0100

    different markers for different categories

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -20,6 +20,7 @@ const addMarkersToMap = (map, markers) => {
     element.className = 'marker';
     element.style.backgroundImage = `url('${marker.image_url}')`;
     element.style.backgroundSize = 'contain';
+    element.style.backgroundRepeat = 'no-repeat';
     element.style.width = '60px';
     element.style.height = '60px';
 
