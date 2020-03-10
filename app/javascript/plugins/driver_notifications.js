3f9834a5abe55b8ff43629f8f2e044315e1dd188
Ashanti Richardson <ashanti.richardson@gmail.com>
2020-03-10 16:43:01 +0100

    Action Cable with Andrea, finally working

--- app/javascript/plugins/driver_notifications.js
+++ app/javascript/plugins/driver_notifications.js
@@ -1,8 +1,9 @@
-const showNotificationToDom = (notification) => {
+const showNotificationToDom = (message) => {
   console.log('got it')
   const modal = document.querySelector('#driver_modal')
   modal.querySelector('.modal-body').innerHTML = `<p>${message.content}</p>`
-  $('#modalbtn').click()
+
+  document.querySelector("#modalbtn").click()
 }
 
 export { showNotificationToDom }
