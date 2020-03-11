83a086f39b5de486c8d0e7ad6f4065bdce2ea1e3
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-03-11 12:22:25 +0100

    fixed error

--- app/javascript/plugins/delivered.js
+++ app/javascript/plugins/delivered.js
@@ -4,26 +4,27 @@ export const orderDelivered = () => {
 
   const button = document.querySelector('#delivered-btn')
 
-  button.addEventListener('click', () => {
+  if (button) {
+    button.addEventListener('click', () => {
 
-    console.log(button.dataset)
-    // fetch(`/api/v1/drivers/${orderId}`, {
-    //   method: 'PATCH',
-    //   headers: {
-    //     'Content-type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     driver: {
-    //       latitude,
-    //       longitude
-    //     }
-    //   })
-    // })
-    // .then(res => res.json())
-    // .then(data => {
-    //   updateDriverOnMap(data)
-    // })
-  })
-
+      console.log(button.dataset)
+      // fetch(`/api/v1/drivers/${orderId}`, {
+      //   method: 'PATCH',
+      //   headers: {
+      //     'Content-type': 'application/json'
+      //   },
+      //   body: JSON.stringify({
+      //     driver: {
+      //       latitude,
+      //       longitude
+      //     }
+      //   })
+      // })
+      // .then(res => res.json())
+      // .then(data => {
+      //   updateDriverOnMap(data)
+      // })
+    })
+  }
 }
 
