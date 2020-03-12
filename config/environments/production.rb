4905a8f2d8dc38c81dceeff0d1f8b741e0d40086
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-03-12 18:45:50 +0100

    actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://packed-right-2/cable"
-  config.web_socket_server_url = "wss://packed-right-2/cable"
+  config.action_cable.url = "wss://packed-right-2.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://packed-right-2.herokuapp.com/cable"
 
   config.action_cable.allowed_request_origins = [
     'http://packed-right-2.herokuapp.com',
